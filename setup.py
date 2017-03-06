a0e7735f228151bf42c8fab0af3db5f66e23297a
Dmitri Cuvircencov <kuvircenkov@gmail.com>
2017-03-06 14:32:04 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(name='thumbor-custom-domain',
+      version='0.0.1',
+      description='Custom domain loader',
+      url='https://github.com/dimetris/thumbor-custom-domain',
+      license='MIT',
+      classifiers=['Intended Audience :: Developers',
+                   'Topic :: Software Development :: Libraries',
+                   'Development Status :: 3 - Alpha',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.3'])
