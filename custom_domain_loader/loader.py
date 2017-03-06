fa81b7331258ee842a09d5c916db08f7c106a456
Dmitri Cuvircencov <kuvircenkov@gmail.com>
2017-03-06 20:27:31 +0200

    replaced : with encoded value

--- custom_domain_loader/loader.py
+++ custom_domain_loader/loader.py
@@ -6,6 +6,6 @@ from thumbor.loaders.http_loader import _normalize_url, load_sync
 
 @return_future
 def load(context, url, callback, normalize_url_func=_normalize_url):
-    if not any([True for schema in ('http://', 'https://') if schema in url]):
+    if not any([True for schema in ('http%3A//', 'https%3A//') if schema in url]):
         url = context.config.LOADER_CUSTOM_DOMAIN + url
     load_sync(context, url, callback, normalize_url_func)
